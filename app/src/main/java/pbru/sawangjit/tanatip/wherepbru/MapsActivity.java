c8af5c2fcd88b5b4b93de10d1717bf4ba2564c59
MpgCoda <tanatip80@gmail.com>
2016-05-27 17:08:24 +0700

    เสร็จวันที่5

--- app/src/main/java/pbru/sawangjit/tanatip/wherepbru/MapsActivity.java
+++ app/src/main/java/pbru/sawangjit/tanatip/wherepbru/MapsActivity.java
@@ -1,7 +1,14 @@
 package pbru.sawangjit.tanatip.wherepbru;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,6 +18,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Calendar;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -18,8 +27,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private int[] iconInts = {R.drawable.build1, R.drawable.build2,
             R.drawable.build3, R.drawable.build4, R.drawable.build5,
             R.drawable.build6,};
-
-
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double myLatADouble, myLngADouble;
+    private boolean gpsABoolean, networkABoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +39,93 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         resultStrings = getIntent().getStringArrayExtra("Result");
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }   //Main method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        locationManager.removeUpdates(locationListener);
+
+        myLatADouble = Double.parseDouble(resultStrings[3]);
+        myLatADouble = Double.parseDouble(resultStrings[4]);
+
+        Location ispLocation = myFindLocation(LocationManager.NETWORK_PROVIDER, "Not Connected Internet");
+
+        if (ispLocation != null) {
+
+            myLatADouble = ispLocation.getAltitude();
+            myLngADouble = ispLocation.getLongitude();
+        }
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER, "No card GPs");
+        if (gpsLocation != null) {
+            myLatADouble = gpsLocation.getAltitude();
+            myLngADouble = gpsLocation.getLongitude();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public  Location myFindLocation(String strProvider,
+                                    String strError) {
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates(strProvider,
+                    1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } else {
+            Log.d("WhereV1", "My Error ==>" + strError);
+        }
+
+        return location;
+    }
+
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            myLatADouble = location.getAltitude();
+            myLngADouble = location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -41,8 +134,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Setup center Map
         setupCenterMap();
 
+        createMakerUser();
+
 
     }   //onMapReady
+
+    private void createMakerUser() {
+
+        LatLng latLng = new LatLng(myLatADouble, myLngADouble);
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.doremon48)));
+
+    }
 
     private void setupCenterMap() {
 
